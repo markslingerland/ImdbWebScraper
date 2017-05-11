b9d5c9bc7d1bae272d4ee250a8807ea65be15209
markslingerland <markslingerland@hotmail.com>
2017-05-11 12:28:25 +0200

    on page load index

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
+app.get('/', function(req, res){
 
 url = 'http://www.imdb.com/title/tt3896198/?ref_=nv_sr_1';
 
